14299bc84e2151f92de5d21f5e539bacc52d8520
Raksana Karlova <raksana.karlova@gmail.com>
2026-05-26 20:21:51 +0400

    add useBoard hook

--- src/hooks/useBoard.ts
+++ src/hooks/useBoard.ts
@@ -16,21 +16,61 @@ export function useBoard() {
     setColumns(result)
   }
 
-  function moveCard() {}
+  function moveCard(
+    cardId: string,
+    fromColumnId: string,
+    toColumnId: string
+  ): void {
+    const foundColumn = columns.find((column) => column.id === fromColumnId);
+    const foundCard = foundColumn?.cards.find((card) => card.id === cardId);
 
-  function editCard() {}
+    if (!foundCard) return;
+
+    const result = columns.map((column) => {
+      if (column.id === fromColumnId) {
+        return {
+          ...column,
+          cards: column.cards.filter((card) => card.id !== cardId),
+        }
+      }
+      if (column.id === toColumnId) {
+        return { ...column, cards: [...column.cards, foundCard] }
+      }
+      return column
+    })
+    setColumns(result)
+  }
+
+  function editCard(
+    cardId: string,
+    columnId: string,
+    updateData: Partial<Omit<Card, 'id'>>
+  ): void {
+    const result = columns.map((column) =>
+      column.id === columnId
+        ? {
+          ...column,
+          cards: column.cards.map((card) =>
+            card.id === cardId ? { ...card, ...updateData } : card
+          ),
+        }
+        : column
+    );
+
+    setColumns(result);
+  }
 
   function deleteCard(cardId: string, columnId: string): void {
     const result = columns.map((column) =>
       column.id === columnId
         ? {
-            ...column,
-            cards: column.cards.filter((card) => card.id !== cardId),
-          }
+          ...column,
+          cards: column.cards.filter((card) => card.id !== cardId),
+        }
         : column
-    )
-    setColumns(result)
+    );
+    setColumns(result);
   }
 
-  return { columns, addCard, moveCard, editCard, deleteCard }
-}
+  return { columns, addCard, moveCard, editCard, deleteCard };
+};
